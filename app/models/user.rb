626ac258409b26cc2e13b372dbfdec5d4bff81b0
DadaLV <vynohradova.l.a@gmail.com>
2023-05-31 09:48:46 +0200

    add full_name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :phone, uniqueness: true
+  validates :full_name, presence: true
   enum role: { admin: "admin", doctor: "doctor", patient: "patient" }
 
   def has_role?(role)

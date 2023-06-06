28df92017cfac866dcf8cf2f023a54d408bd11d3
DadaLV <vynohradova.l.a@gmail.com>
2023-06-06 07:16:02 +0200

    edit navbar

--- spec/features/recommendation_feature_spec.rb
+++ spec/features/recommendation_feature_spec.rb
@@ -5,15 +5,17 @@ RSpec.describe "Recommendation Feature", type: :feature do
   before do
     doctor = Doctor.create(full_name: "John Doe", phone: "1234567890", password: "password")
     patient = Patient.create(full_name: "Jane Smith", phone: "9876543210", password: "password")
-    login_as(doctor, scope: :user)
+    sign_in(doctor)
   end
 
   it "leaves a recommendation for a patient" do
-    visit "/index"
+    visit root_path
+    
+
     click_link "View Appointments"
     click_link "Leave Recommendation"
     fill_in "recommendation_content", with: "Take pills!"
-    click_button "Submit"
+    click_button "Leave Recommendation"
     expect(page).to have_content("Appointment closed.")
   end
 end
